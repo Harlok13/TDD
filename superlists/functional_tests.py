830ff7d6f965a5d0254b01842a7e0a9b23bc439a
Harlok <holmik90@yandex.ru>
2022-12-29 16:08:16 +0300

    ФТ тест теперь проверяет возможность ввести элемент списка

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -22,18 +24,32 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do lists', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element('h1').text
+        self.assertIn('To-Do lists', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
         # вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element('id_list_table')
+        rows = table.find_elements('tr')
+        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows))
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
@@ -42,6 +58,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
+
 
 
 if __name__ == '__main__':
